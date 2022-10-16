ff6aff4060065ba6a390f2bfc6f8128388c415d5
Babasky <boureimamaiga67@gmail.com>
2022-10-16 07:12:40 +0200

    react

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header';
+import Banner from './components/Banner';
+import ServicesExperience from './components/ServicesExperience';
+import Projets from './components/Projets';
+import Footer from './components/Footer';
+function App() {
+  return (
+    <div>
+      <Header/>
+      <Banner/>
+      <ServicesExperience/>
+      <Projets/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
